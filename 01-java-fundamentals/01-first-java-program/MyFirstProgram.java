b92b657edcae8c0f024e91bf3547a9c732dd70cf
Mike Young <mike.young@salesforce.com>
2020-08-03 23:40:44 -0500

    First Java Program

---
+++ 01-java-fundamentals/01-first-java-program/MyFirstProgram.java
@@ -0,0 +1,12 @@
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        String name = "My name is Coding Dojo";
+        int age = 100;
+        String hometown = "My hometown is Burbank, CA";
+
+        System.out.println(name);
+        System.out.println("I am " + age + " years old");
+        System.out.println(hometown);
+    }
+}
