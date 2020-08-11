dbf370dcb6e8b53bc7a57b9df0586dc46ab3c076
Mike Young <mike.young@salesforce.com>
2020-08-10 22:48:46 -0500

    Project Assignment

---
+++ 02-java-oop/01-project-class/Project.java
@@ -0,0 +1,39 @@
+public class Project{
+    private String name;
+    private String description;
+
+    public Project() {
+    }
+
+    //overload the method 2)
+    public Project(String name) {
+        this.name = name;
+    }
+
+    //overload the method 3)
+    public Project(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+      public String setDescription() {
+        return this.description;
+    }
+
+    //create elevatorPitch
+    public String elevatorPitch() {
+        return this.name + ":" + this.description;
+    }
+}
---
+++ 02-java-oop/01-project-class/ProjectTest.java
@@ -0,0 +1,9 @@
+public class ProjectTest{
+    public static void main(String[] args) {
+        Project project =  new Project();
+        project.setName("Mike");
+        project.setDescription("Schoolwork");
+
+    System.out.println(project.elevatorPitch());
+    }
+}
